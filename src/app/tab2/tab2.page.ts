f1ff21a619db0838986edb6a93a9687e9998f392
jm_b <jmbanchero@gmail.coom>
2020-07-01 18:40:59 +0100

    add camara

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
+import { PhotoService } from '../services/photo.service';
 
 @Component({
   selector: 'app-tab2',
   templateUrl: 'tab2.page.html',
-  styleUrls: ['tab2.page.scss']
+  styleUrls: ['tab2.page.scss'],
 })
 export class Tab2Page {
-
-  constructor() {}
-
+  constructor(public photoService: PhotoService) {}
 }
